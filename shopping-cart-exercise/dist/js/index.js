bc997a2831e9a1f081540379946bc9079c3a9241
isabellaes <borjessonisabella@gmail.com>
2023-12-14 11:36:53 +0100

    first task finished

--- shopping-cart-exercise/dist/js/index.js
+++ shopping-cart-exercise/dist/js/index.js
@@ -2,10 +2,28 @@ const productButtons = document.querySelectorAll("button");
 const shoppingCart = [];
 const cart = document.querySelector("#cart");
 const openCartButton = document.querySelector("#open-cart");
-function updateCart() { }
+const cartNumber = document.getElementById("productsInCart");
+function updateCart(button) {
+    shoppingCart.push(button.parentElement.dataset.product);
+    listProductsInCart(button.parentElement.dataset.product);
+    cartNumber.innerHTML = shoppingCart.length.toString();
+}
 function addClickEvent() {
     // Void betyder att funktionen ej returnerar något värde
     // För att kunna använda parentElement behöver vi göra om det till ett HTMLElement istället för typen EventTarget
+    productButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            updateCart(button);
+        });
+    });
+    openCartButton.addEventListener("click", () => {
+        document.querySelector(".cart-wrapper").classList.toggle("hide");
+    });
 }
-function listProductsInCart() { }
+function listProductsInCart(button) {
+    let list = cart.querySelector("ul");
+    const product = document.createElement("li");
+    product.innerHTML = button;
+    list.appendChild(product);
+}
 addClickEvent();
--- shopping-cart-exercise/src/index.ts
+++ shopping-cart-exercise/src/index.ts
@@ -2,14 +2,33 @@ const productButtons: NodeList = document.querySelectorAll("button");
 const shoppingCart: (string | null)[] = [];
 const cart: HTMLElement | null = document.querySelector("#cart");
 const openCartButton: HTMLElement | null = document.querySelector("#open-cart");
+const cartNumber: HTMLElement = document.getElementById("productsInCart");
 
-function updateCart(): void {}
+function updateCart(button): void {
+  shoppingCart.push(button.parentElement.dataset.product);
+  listProductsInCart(button.parentElement.dataset.product);
+  cartNumber.innerHTML = shoppingCart.length.toString();
+}
 
 function addClickEvent(): void {
   // Void betyder att funktionen ej returnerar något värde
   // För att kunna använda parentElement behöver vi göra om det till ett HTMLElement istället för typen EventTarget
+  productButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      updateCart(button);
+    });
+  });
+
+  openCartButton.addEventListener("click", () => {
+    document.querySelector(".cart-wrapper").classList.toggle("hide");
+  });
 }
 
-function listProductsInCart(): void {}
+function listProductsInCart(button): void {
+  let list = cart.querySelector("ul");
+  const product = document.createElement("li");
+  product.innerHTML = button;
+  list.appendChild(product);
+}
 
 addClickEvent();
